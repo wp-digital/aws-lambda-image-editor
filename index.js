deb74177c466f17fe9679c7b04c47c4c8b6faa7b
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2018-03-09 13:07:02 +0200

    update packages

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-var gm = require('gm').subClass({imageMagick: true});
-var Imagemin = require('imagemin');
+var gm = require('gm').subClass({
+    imageMagick: true
+});
+var imagemin = require('imagemin');
 var imageminJpegoptim = require('imagemin-jpegoptim');
 var imageminPngquant = require('imagemin-pngquant');
+var imageminGifsicle = require('imagemin-gifsicle');
+var imageminSvgo = require('imagemin-svgo');
 
 var aws = require('aws-sdk');
-var s3 = new aws.S3({ apiVersion: '2006-03-01' });
+var s3 = new aws.S3({
+    apiVersion: '2006-03-01'
+});
 
 var mime = require('mime');
 
@@ -55,7 +61,6 @@ function do_operations(image, operations) {
 }
 
 exports.handler = function( event, context ) {
-
     var bucket = event.bucket;
     var filename = event.filename;
     var new_filename = event.new_filename;
@@ -68,44 +73,55 @@ exports.handler = function( event, context ) {
         Bucket: bucket,
         Key: filename
     }, function(err, data) {
-        if (err) throw err;
+        if (err) {
+            throw err;
+        }
+
         var meta = data.Metadata;
         var image = gm(new Buffer(data.Body, 'binary'), filename);
+
         image.quality(quality);
         do_operations(image, operations);
 
         image.toBuffer( new_filename, function(err, buffer) {
             if (err) throw err;
 
-            new Imagemin()
-                .src(buffer)
-                .use(imageminJpegoptim({progressive: true, max: quality}))
-                .use(Imagemin.gifsicle({interlaced: true}))
-                .use(imageminPngquant({quality: (quality - 10) + '-' + quality, speed: 4}))
-                .use(Imagemin.svgo())
-                .run(function (err, files) {
-                    if (err) throw err;
-
-                    if(event.return === 'stream') {
-                        context.succeed(new Buffer(files[0].contents).toString('base64'));
-                    }
-                    else {
-                        s3.putObject({
-                            Bucket: bucket,
-                            Key: new_filename,
-                            Body: files[0].contents,
-                            //ACL: acl, todo: clone acl
-                            ContentType: mime.lookup(new_filename),
-                            Metadata: meta
-                        }, function(err, data) {
-                            if (err) throw err;
-                            context.done();
-                        });
-                    }
-
-                });
+            imagemin.buffer(buffer, {
+                plugins: [
+                    imageminJpegoptim({
+                        progressive: true,
+                        max: quality
+                    }),
+                    imageminGifsicle({
+                        interlaced: true
+                    }),
+                    imageminPngquant({
+                        quality: (quality - 10) + '-' + quality,
+                        speed: 4
+                    }),
+                    imageminSvgo({
+                        plugins: [{
+                            removeViewBox: false
+                        }]
+                    })
+                ]
+            }).then(function (body) {
+                if(event.return === 'stream') {
+                    context.succeed(body.toString('base64'));
+                } else {
+                    s3.putObject({
+                        Bucket: bucket,
+                        Key: new_filename,
+                        Body: body,
+                        //ACL: acl, todo: clone acl
+                        ContentType: mime.getType(new_filename),
+                        Metadata: meta
+                    }, function(err) {
+                        if (err) throw err;
+                        context.done();
+                    });
+                }
+            });
         });
-
     });
-
 };
