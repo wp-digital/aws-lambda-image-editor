6d7a3bc96f49c8246071fc01f9e0a9726cf18e69
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2019-05-02 15:44:57 +0300

    pnquant param type fix

--- index.js
+++ index.js
@@ -173,7 +173,7 @@ exports.handler = ({
                 max: quality
             }),
             imageminPngquant({
-                quality: (quality - 10) + '-' + quality,
+                quality: [(quality - 10), quality],
                 speed: 4
             }),
             imageminSvgo({
